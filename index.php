f019375c1d0e2e729d528690c5628facde781462
xiaoting819 <405115149@qq.com>
2024-04-25 17:36:09 +0800

    更新下头部样式调整。
    Signed-off-by: xiaoting819 <405115149@qq.com>

--- index.php
+++ index.php
@@ -2150,7 +2150,7 @@ function fm_show_header($nav,$path)
         body {
       	    font-feature-settings: "cv03", "cv04", "cv11";
         }
-        .nav-item{min-width: 92px;}
+        .nav-item-n{min-width: 92px;}
         .btn-icon1 {
             min-width:calc(var(--tblr-btn-line-height) * var(--tblr-btn-font-size) + var(--tblr-btn-padding-y) * 1.2 + var(--tblr-btn-border-width) * 1.2);
             min-height:calc(var(--tblr-btn-line-height) * var(--tblr-btn-font-size) + var(--tblr-btn-padding-y) * 1.2 + var(--tblr-btn-border-width) * 1.2);
@@ -2509,7 +2509,7 @@ function fm_show_header($nav,$path)
                 <button type="submit"   class="btn btn-success ms-auto" data-bs-dismiss="modal">
                     <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
                     <input type="hidden" name="user_hash" value="">
-                    <?php echo lng('Save') ?> 
+                    <?php echo lng('Save') ?>
                 </button>
             </div>
             </form>
@@ -2633,12 +2633,10 @@ function fm_show_header($nav,$path)
             <div class="d-none d-md-flex">
               <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="Enable dark mode" data-bs-toggle="tooltip"
 		   data-bs-placement="bottom">
-                <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
                 <svg xmlns="<?php print_external('icon-2000');?>" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>
               </a>
               <a href="?theme=light" class="nav-link px-0 hide-theme-light" title="Enable light mode" data-bs-toggle="tooltip"
 		   data-bs-placement="bottom">
-                <!-- Download SVG icon from http://tabler-icons.io/i/sun -->
                 <svg xmlns="<?php print_external('icon-2000');?>" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
               </a>
             </div>
@@ -2649,7 +2647,6 @@ function fm_show_header($nav,$path)
                 </span>
                 <div class="d-none d-xl-block ps-2">
                   <div><?php echo $_SESSION[FM_SESSION_ID]['logged'];?></div>
-                  <!-- <div class="mt-1 small text-muted">UI Designer</div> -->
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
@@ -2665,7 +2662,7 @@ function fm_show_header($nav,$path)
           <div class="navbar">
             <div class="container-xl">
               <ul class="navbar-nav">
-                <li class="nav-item <?php echo $nav=='home'?'active':'';?>">
+                <li class="nav-item nav-item-n <?php echo $nav=='home'?'active':'';?>">
                   <a class="nav-link" href="?p=" >
                     <span class="nav-link-icon d-md-none d-lg-inline-block">
                       <svg xmlns="<?php print_external('icon-2000');?>" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
@@ -2675,7 +2672,7 @@ function fm_show_header($nav,$path)
                     </span>
                   </a>
                 </li>
-                <li class="nav-item <?php echo $nav=='users'?'active':'';?>">
+                <li class="nav-item nav-item-n <?php echo $nav=='users'?'active':'';?>">
                   <a class="nav-link" href="?nav=users" >
                     <span class="nav-link-icon d-md-none d-lg-inline-block">
                     <svg xmlns="<?php print_external('icon-2000');?>" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path><path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path></svg>
@@ -2685,7 +2682,7 @@ function fm_show_header($nav,$path)
                     </span>
                   </a>
                 </li>
-                <li class="nav-item <?php echo $nav=='logs'?'active':'';?>">
+                <li class="nav-item nav-item-n <?php echo $nav=='logs'?'active':'';?>">
                   <a class="nav-link" href="?nav=logs" >
                     <span class="nav-link-icon d-md-none d-lg-inline-block">
                         <svg xmlns="<?php print_external('icon-2000');?>" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 19h-6a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1h6a2 2 0 0 1 2 2a2 2 0 0 1 2 -2h6a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-6a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2z"></path><path d="M12 5v16"></path><path d="M7 7h1"></path><path d="M7 11h1"></path><path d="M16 7h1"></path><path d="M16 11h1"></path><path d="M16 15h1"></path></svg>
@@ -2695,7 +2692,7 @@ function fm_show_header($nav,$path)
                     </span>
                   </a>
                 </li>
-                <li class="nav-item <?php echo $nav=='settings'?'active':'';?>">
+                <li class="nav-item  nav-item-n <?php echo $nav=='settings'?'active':'';?>">
                   <a class="nav-link" href="?nav=settings" >
                     <span class="nav-link-icon d-md-none d-lg-inline-block">
                         <svg xmlns="<?php print_external('icon-2000');?>" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path></svg>
